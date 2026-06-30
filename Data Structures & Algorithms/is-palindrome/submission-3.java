f92a745143e1dee35f650fedb7205730e08b8caf
Sanchita Mali <149798493+Sanchitaaa03@users.noreply.github.com>
2026-06-30 14:56:58 +0530

    Add: is-palindrome - submission-3

---
+++ Data Structures & Algorithms/is-palindrome/submission-3.java
@@ -0,0 +1,30 @@
+class Solution {
+    public boolean isPalindrome(String s) {
+
+        StringBuilder sb = new StringBuilder();
+
+        for(int i=0;i<s.length();i++){
+                char ch=s.charAt(i);
+                if(ch >= 'a' && ch <='z' || ch >='A' && ch<='Z' || ch>='0' && ch<='9'){
+                    sb.append(Character.toLowerCase(ch));
+                }
+        }
+
+        int i=0;
+        int j=sb.length()-1;
+
+        while(i<=j){
+            if(sb.charAt(i)!=sb.charAt(j)){
+                return false;
+            }
+            i++;
+            j--;
+        }
+        return true;
+    }
+        
+
+
+
+    
+}
