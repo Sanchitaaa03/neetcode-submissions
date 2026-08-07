e4e30346d71b7f6eb9742d7f9c4025802ac30151
Sanchita Mali <149798493+Sanchitaaa03@users.noreply.github.com>
2026-08-08 02:27:18 +0530

    Add: search-2d-matrix - submission-4

---
+++ Data Structures & Algorithms/search-2d-matrix/submission-4.java
@@ -0,0 +1,37 @@
+class Solution {
+    public boolean searchMatrix(int[][] matrix, int target) {
+
+        int i=0;
+        
+        for(i=0;i<matrix.length;i++){
+            int j=matrix[i].length-1;
+
+            if(matrix[i][j]>=target){
+                break;
+            }
+        }
+
+        if(matrix.length==i){
+            return false;
+        }
+        
+
+        int low=0;
+        int high=matrix[i].length-1;
+
+        while(low<=high){
+            int mid= (low+high)/2;
+
+            if(matrix[i][mid]==target){
+                return true;
+            }
+            else if(matrix[i][mid]<target){
+                low=mid+1;
+            }else
+            {
+                high=mid-1;
+            }
+        }
+        return false;
+    }
+}
